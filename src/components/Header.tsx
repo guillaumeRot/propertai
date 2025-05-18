a09ebbc4d0bccd0e9e13b3825bb2abeaf1ef69aa
Guillaume <guillaume.rot@gmail.com>
2025-05-18 17:08:10 +0200

    feat: link in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,19 +6,18 @@ export default function Header() {
     <header className="fixed top-0 left-0 right-0 backdrop-blur-2xl z-50 border-b border-gray-100 transform transition-transform duration-500">
       <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 h-20 flex items-center justify-between">
         <div className="flex items-center">
-          {/* <span className="text-3xl font-[600] tracking-tight text-blue-500">
-            PropertAI
-          </span> */}
-          <Image
-            src="/logo_orange_bleu.png"
-            alt="Logo"
-            width={200}
-            height={200}
-            className="ml-2"
-          />
+          <a href="/">
+            <Image
+              src="/logo_orange_bleu.png"
+              alt="Logo"
+              width={200}
+              height={200}
+              className="ml-2"
+            />
+          </a>
         </div>
         <Link
-          href="/signup"
+          href="/analyse"
           className="px-6 py-2 bg-orange-500 text-white rounded-full hover:bg-orange-600 transition-colors duration-200 font-medium"
         >
           Tester l'outil
