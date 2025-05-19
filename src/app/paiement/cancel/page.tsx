8d42ad44b542d71ee55fb5c4b537211331ef7ae7
Guillaume <guillaume.rot@gmail.com>
2025-05-19 23:37:18 +0200

    feat: page success and cancel

--- src/app/paiement/cancel/page.tsx
+++ src/app/paiement/cancel/page.tsx
@@ -1,13 +1,27 @@
-export default function PaiementSuccess() {
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import Link from "next/link";
+
+export default function PaiementCancel() {
   return (
-    <div className="p-8 text-center">
-      <h1 className="text-2xl font-bold text-green-600 mb-4">
-        Paiement échoué
-      </h1>
-      <p className="text-gray-700">
-        Merci pour votre abonnement ! Vous pouvez maintenant utiliser toutes les
-        fonctionnalités de PropertAI.
-      </p>
-    </div>
+    <>
+      <Header />
+      <div className="min-h-screen flex flex-col justify-center items-center bg-gray-50 p-8">
+        <h1 className="text-3xl font-bold text-red-600 mb-4">
+          Paiement annulé ❌
+        </h1>
+        <p className="text-gray-700 text-center max-w-md mb-6">
+          Vous avez annulé le processus de paiement. Aucun montant n’a été
+          débité. Vous pouvez réessayer à tout moment si vous changez d’avis.
+        </p>
+        <Link
+          href="/abonnement-annuel"
+          className="text-orange-500 hover:underline"
+        >
+          Revenir à l’abonnement
+        </Link>
+      </div>
+      <Footer />
+    </>
   );
 }
--- src/app/paiement/success/page.tsx
+++ src/app/paiement/success/page.tsx
@@ -1,13 +1,20 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+
 export default function PaiementSuccess() {
   return (
-    <div className="p-8 text-center">
-      <h1 className="text-2xl font-bold text-green-600 mb-4">
-        Paiement réussi ✅
-      </h1>
-      <p className="text-gray-700">
-        Merci pour votre abonnement ! Vous pouvez maintenant utiliser toutes les
-        fonctionnalités de PropertAI.
-      </p>
-    </div>
+    <>
+      <Header />
+      <div className="p-8 text-center">
+        <h1 className="text-2xl font-bold text-green-600 mb-4">
+          Paiement réussi ✅
+        </h1>
+        <p className="text-gray-700">
+          Merci pour votre abonnement ! Vous pouvez maintenant utiliser toutes
+          les fonctionnalités de PropertAI.
+        </p>
+      </div>
+      <Footer />
+    </>
   );
 }
