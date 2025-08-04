c4724e6195d4224ce7c80a0bfbe0053ff4d298ab
Guillaume <guillaume.rot@gmail.com>
2025-08-04 22:45:13 +0200

    fix: hotjar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
         ></script>
         <HotJar />
         <script>
-          (function(h,o,t,j,a,r){
+          {`(function(h,o,t,j,a,r){
               h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
               h._hjSettings={hjid:6484019,hjsv:6};
               a=o.getElementsByTagName('head')[0];
@@ -46,7 +46,8 @@ export default function RootLayout({
               r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
               a.appendChild(r);
           })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-      </script>
+        `}
+        </script>
       </head>
       <body className={inter.className}>
         <Providers>{children}</Providers>
